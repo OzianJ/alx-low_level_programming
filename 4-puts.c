ff995f01d81ad7949109d910b5106a03e7b6a249
OzianJ <eskinderteferi12@gmail.com>
2023-03-16 09:03:28 -0700

    printing with puts function

---
+++ 4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ *puts- a library function to send formatted outputs to the screen
+ *int main(void)- the entry point of the function
+ *Return- exit status of the program which returns 0 if successful
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle\n");
+  return (0);
+}
