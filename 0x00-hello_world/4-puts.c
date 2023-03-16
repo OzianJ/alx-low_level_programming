685e0d8a2e474c6e48e440fc2b262c06a9720ab3
OzianJ <eskinderteferi12@gmail.com>
2023-03-16 09:57:12 -0700

    modified puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /*
- *main function: the entry point of the program
  *puts function: a library function to send formatted output to the screen
  *Return: the exit status of the program whcich returns 0 if sucessful
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
