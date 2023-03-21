8d28292626fa5770db7a7c102c7046004dd5a138
OzianJ <eskinderteferi12@gmail.com>
2023-03-21 12:33:00 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+ * _abs - entry point
+ * Description 'returns the absolute value of intigers'
+ *@a: the intiger to be returned
+ * return: 0 if successful
+ * Return: Void.
+ */
+int _abs(int a)
+{
+if (a < 0)
+{
+return (a * -1);
+}
+return (a);
+}
