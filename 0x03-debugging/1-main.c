0accad3fa405532b5daaeda9bc3f22b2e85d4a22
OzianJ <eskinderteferi12@gmail.com>
2023-03-23 07:35:58 -0700

    task 1 updated

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* Description 'comment out causes of infinite loop'
-*@\\o/\n - infinite loop avoided
-*@{ - the commented while loop
-* Return: 0
+* main - Entry Point
+* Description: 'cause an infinite loop'
+* '\\o/\n"':'the end of a string'
+* Return: 0 (Always Sucess)
 */
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
+
 i = 0;
-/**
- * while (i < 10)
- * {
- * putchar(i);
- * }
- */
+
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
