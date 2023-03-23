5f2016ef6b2a80c5cf8b633449a843de92a5400b
OzianJ <eskinderteferi12@gmail.com>
2023-03-23 10:42:58 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Description: 'multiply two digits'
+ * @a: first digit
+ * @b: second digit
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+  return (a * b);
+}
