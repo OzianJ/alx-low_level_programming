c3ac237b4298fb1a2716b80c1e4a50b017d8579e
OzianJ <eskinderteferi12@gmail.com>
2023-03-23 10:58:56 -0700

    print from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * print_numbers - entry point
+ * Description: 'print numbers from 0 to 9'
  * Return: Always 0.
  */
 
@@ -14,4 +14,4 @@ for (i = '0'; i <= '9'; i++)
 _putchar(i + '0');
 }
 _putchar('\n')
-} 
+}
