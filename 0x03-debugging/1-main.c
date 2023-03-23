f8b740dfbdac9ea13cab5f7230f1e315f4d00a83
OzianJ <eskinderteferi12@gmail.com>
2023-03-23 06:40:13 -0700

    avoid infinite loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description 'comment out causes of infinite loop'
+*@\\o/\n - infinite loop avoided
+*@{ - the commented while loop
+* Return: 0
+*/
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+/**
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ */
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
