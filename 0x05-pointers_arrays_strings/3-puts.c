aff403dfef7b7c798bad50e49c3b3a7969603d8f
OzianJ <eskinderteferi12@gmail.com>
2023-03-28 06:15:31 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - entry point
+ * Description:'prints string to stdout, followed by new line
+ *@str: the string to be printed
+ */
+
+void _puts(char *str)
+{
+int a;
+while (a = 0, str[a] != '\0')
+{   
+_putchar(str[a]);
+a++;
+}
+_putchar('\n');
+}
