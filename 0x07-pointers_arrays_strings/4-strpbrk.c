5dcce8b29b78f628574da2e8a85a2ba67aa5ed6c
OzianJ <eskinderteferi12@gmail.com>
2023-04-04 10:55:50 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry Point
+ *Description:'function that searches a string for any of a set of bytes'
+ *@s: first value -char
+ *@accept: second value - char
+ *
+ * Return: char with result
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i = 0;
+while (*s)
+{
+while (accept[i] != '\0')
+{
+if (*s == accept[i])
+return (s);
+i++;
+}
+i = 0;
+s++;
+}
+return ('\0');
+}
